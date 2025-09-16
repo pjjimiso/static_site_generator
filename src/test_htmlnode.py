4a10381a458bcd04d93c7910e2948769c06891a9
Patrick Jimison <patrick.jimison@altera.com>
2025-09-16 15:15:09 -0700

    Cleaned up HTMLNode unittests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -6,26 +6,34 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_htmlnode_eq(self):
-        node = HTMLNode("p", "some text inside a paragraph")
-        node2 = HTMLNode("p", "some text inside a paragraph")
-        self.assertEqual(node, node2)
+    def test_values(self):
+        child_node = HTMLNode(
+            "a",
+            "Website", 
+            None, 
+            {"href": "https://thumpertherapy.com", "target": "_blank",}
+        )
+        parent_node = HTMLNode("li", "this is a list", [child_node])
+        self.assertEqual(parent_node.tag, "li")
+        self.assertEqual(parent_node.value, "this is a list")
+        self.assertEqual(parent_node.children, [child_node])
+        self.assertEqual(parent_node.props, None)
 
-    def test_htmlnode_neq(self):
-        node = HTMLNode("p", "some text inside a paragraph", None, None)
-        node2 = HTMLNode("p", "different text inside a paragraph", None, None)
-        self.assertNotEqual(node, node2)
+    def test_repr(self):
+        node = HTMLNode("a", "Website", None, {"href": "https://thumpertherapy.com"})
+        self.assertEqual(
+                node.__repr__(), 
+                "HTMLNode(a, Website, children: None, {'href': 'https://thumpertherapy.com'})"
+        )
 
     def test_props_to_html(self):
-        props = {"href": "https://thumpertherapy.com", "target": "_blank",}
-        node = HTMLNode("a", "Website", None, props)
-        test_string = ' href="https://thumpertherapy.com" target="_blank"'
-        html = node.props_to_html()
-        self.assertEqual(html, test_string)
-
-    def test_children_eq(self): 
-        props = {"href": "https://thumpertherapy.com", "target": "_blank",}
-        child_node = HTMLNode("a", "Website", None, props)
-        children = [child_node]
-        parent_node = HTMLNode("li", None, children, None)
-        self.assertEqual(child_node, parent_node.children[0])
+        node = HTMLNode(
+            "a", 
+            "Website", 
+            None, 
+            {"href": "https://thumpertherapy.com", "target": "_blank",}
+        )
+        self.assertEqual(
+            node.props_to_html(), 
+            ' href="https://thumpertherapy.com" target="_blank"'
+        )
