0c5d742d7d10e7c5b41a334d20ce2d0ab9b245b5
Patrick Jimison <patrick.jimison@altera.com>
2025-09-18 16:20:10 -0700

    Add unittests for markdown extraction functions

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,51 @@
+# test_extract_markdown.py
+
+import unittest
+from extract_markdown import (
+        extract_markdown_images,
+        extract_markdown_links,
+)
+
+
+class TestExtractMarkdown(unittest.TestCase): 
+    def test_extract_markdown_images(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)."
+        images = extract_markdown_images(text)
+        self.assertEqual(
+            images,
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+            ]
+        )
+
+    def test_extract_markdown_links(self):
+        text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        links = extract_markdown_links(text)
+        self.assertEqual(
+            links,
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev")
+            ]
+        )
+    
+    def test_extract_markdown_combined(self):
+        text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg). This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        links = extract_markdown_links(text)
+        images = extract_markdown_images(text)
+        self.assertEqual(
+            links,
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev")
+            ]
+        )
+        self.assertEqual(
+            images,
+            [
+                ("rick roll", "https://i.imgur.com/aKaOqIh.gif"),
+                ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")
+            ]
+        )
+
