f29da211b97f16a9bab81fec180ee1612db16a9a
Patrick Jimison <patrick.jimison@altera.com>
2025-09-19 10:20:05 -0700

    added function for converting markdown document into blocks

--- src/extract_markdown.py
+++ src/extract_markdown.py
@@ -12,3 +12,13 @@ def extract_markdown_links(text):
     matches = re.findall(r'(?<!!)\[([\w\d\s]*)]\((\S*)\)', text)
     return matches
 
+def markdown_to_blocks(markdown): 
+    final_blocks = []
+    blocks = markdown.split("\n\n")
+    for block in blocks: 
+        if block == "":
+            continue
+        stripped_block = block.strip()
+        final_blocks.append(stripped_block)
+    return final_blocks
+
