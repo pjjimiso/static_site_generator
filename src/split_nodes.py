d1004cae75bc13cadf09ee121f4e2341cb6e7d2b
Patrick Jimison <patrick.jimison@altera.com>
2025-09-21 09:51:59 -0700

    Remove debug output

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -89,6 +89,3 @@ def text_to_textnodes(text):
     new_nodes = split_nodes_link(new_nodes)
     return new_nodes
 
-
-text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-text_to_textnodes(text)
