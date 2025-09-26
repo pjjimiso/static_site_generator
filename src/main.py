1f546226ee87aff3eebde84f7a34bb0326a705af
Patrick Jimison <patrick.jimison@altera.com>
2025-09-25 18:06:31 -0700

    Add recursive page generation

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from textnode import TextType, TextNode
 from split_nodes import split_nodes_delimiter
 from extract_markdown import extract_markdown_title
 from markdown_blocks import markdown_to_html_node
+from pathlib import Path
 import logging
 import os
 import shutil
@@ -17,6 +18,7 @@ def setup_logging(file):
         filemode='w',
         format='%(asctime)s %(levelname)s: %(message)s'
     )
+
 
 def copy_files(src, dst, level=1):
     if os.path.exists(dst) and level == 1: 
@@ -33,6 +35,7 @@ def copy_files(src, dst, level=1):
             subdir = os.path.join(dst, file)
             copy_files(src=file_path, dst=subdir, level=level+1)
 
+
 def read_file_contents(file): 
     try:
         with open(file, 'r') as f: 
@@ -43,6 +46,7 @@ def read_file_contents(file):
         print(f"An error occured {e}")
     return  contents
 
+
 def write_file_contents(file, contents): 
     try:
         with open(file, 'w') as f: 
@@ -52,20 +56,38 @@ def write_file_contents(file, contents):
     except Exception as e: 
         print(f"An error occured {e}")
 
+
 def generate_page(from_path, template_path, dest_path): 
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     markdown_contents = read_file_contents(from_path)
     title = extract_markdown_title(markdown_contents)
     logging.debug(f"Page title: {title}")
-    logging.debug(f"printing contents of markdown file:\n{markdown_contents}")
     html = markdown_to_html_node(markdown_contents).to_html()
     template_contents = read_file_contents(template_path)
     contents = re.sub(r'{{\s*Title\s*}}', title, template_contents)
-    logging.debug(f"Replace Title with {title}:\n{contents}")
     contents = re.sub(r'{{\s*Content\s*}}', html, contents)
-    logging.debug(f"Replace Contents:\n{contents}")
-    write_file_contents(dest_path, contents)
-    logging.debug(f"printing contents to index.html:\n{dest_path}")
+    #if not os.path.exists(dest_path):
+    #    logging.debug(f"Creating directory {dest_path}")
+    #    os.mkdir(dest_path)
+    file_path = Path(from_path)
+    dest_file_name = file_path.stem + ".html"
+    dest_file = os.path.join(dest_path, dest_file_name)
+    logging.debug(f"Writing contents to {dest_file}")
+    write_file_contents(dest_file, contents)
+
+
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
+    for file in os.listdir(dir_path_content):
+        file_path = os.path.join(dir_path_content, file)
+        if os.path.isfile(file_path) and file_path.endswith(".md"):
+            print(f"we've found a markdown file to generate: {file_path}")
+            generate_page(file_path, template_path, dest_dir_path)
+        elif os.path.isdir(file_path):
+            print(f"descending into directory {file_path}")
+            dest_subdir_path = os.path.join(dest_dir_path, file)
+            if not os.path.exists(dest_subdir_path):
+                os.mkdir(dest_subdir_path)
+            generate_page_recursive(file_path, template_path, dest_subdir_path)
 
 
 def main(): 
@@ -73,7 +95,8 @@ def main():
     source = "/home/pjjimiso/Projects/bootdotdev/static_site_generator/static"
     destination = "/home/pjjimiso/Projects/bootdotdev/static_site_generator/public"
     copy_files(src=source, dst=destination)
-    generate_page(from_path="content/index.md", template_path="template.html", dest_path="public/index.html")
+    generate_page_recursive("content", "template.html", "public")
+    #generate_page(from_path="content/index.md", template_path="template.html", dest_path="public/index.html")
 
 
 if __name__ == "__main__": 
